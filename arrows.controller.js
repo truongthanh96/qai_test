8a7651670b9f7f8a2cee9e659d2ea02a312f1bad
Vinh Pham <vinh@quod.ai>
2019-12-27 15:00:38 +0700

    Add module.exports arrow function

--- arrows.controller.js
+++ arrows.controller.js
@@ -6,3 +6,9 @@ app.get("/arrows/:arrowId", (req, res) => {
     message: "I'm an arrow function"
   })
 })
+
+module.exports = (app) => {
+       app.get('/users/:userId', (req, res) => {
+         res.send({ "message": "Hello world" });
+       });
+    }
