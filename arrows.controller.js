1aed40b0c8447cca2e240b6626a71414f0700c7a
Vinh Pham <vinh@quod.ai>
2019-12-28 12:26:28 +0700

    Update module export

--- arrows.controller.js
+++ arrows.controller.js
@@ -1,14 +1,16 @@
 import express from 'express';
 const app = express();
 
+module.exports = (app) => {
+       app.get('/users/:userId', (req, res) => {
+         res.send({ "message": "Hello world" });
+       });
+    }
+
 app.get("/arrows/:arrowId", (req, res) => {
   res.send({
     message: "I'm an arrow function"
   })
 })
 
-module.exports = (app) => {
-       app.get('/users/:userId', (req, res) => {
-         res.send({ "message": "Hello world" });
-       });
-    }
+
