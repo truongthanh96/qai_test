40cbf4b2cfe7e2c8fa931e0d9195a8bdd858bc3d
phamvinh1712 <phamvinh1712@gmail.com>
2020-01-08 14:03:45 +0700

    update line end test file

--- line_end_test.js
+++ line_end_test.js
@@ -1,4 +1,12 @@
-app.get('/users/:userId', function (req, res) {
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express.default)();
+
+    app.get('/users/:userId', function (req, res) {
     res.send({
         "message": "Hello world"
     });
